0d9a90c5ff3c0b72336db7f96c741390452da6d4
kic0412 <kic0412@naver.com>
2023-06-26 17:56:19 +0900

    파이어베이스 연결

--- app/src/main/java/kr/co/kkm/stepgameproject/MainActivity.kt
+++ app/src/main/java/kr/co/kkm/stepgameproject/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.profile)
+        setContentView(R.layout.main)
     }
 }
