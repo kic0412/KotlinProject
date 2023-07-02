fddefa14c197f039ba187bf324515563dfdd612c
Sujin_Lim <lsj3369@gmail.com>
2023-07-02 21:32:33 +0900

    레이아웃 약간 수정

--- app/src/main/java/kr/co/kkm/stepgameproject/ContentActivity.kt
+++ app/src/main/java/kr/co/kkm/stepgameproject/ContentActivity.kt
@@ -3,7 +3,6 @@ package kr.co.kkm.stepgameproject
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class ContentActivity : AppCompatActivity() {
@@ -11,25 +10,36 @@ class ContentActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.content)
 
+        map()
         record()
-        profile()
+        setting()
+    }
+
+    private fun map() {
+        val mapBtn = findViewById<Button>(R.id.contentMapBtn)
+
+        mapBtn.setOnClickListener {
+            val mPage = Intent(this, MapsActivity::class.java)
+            startActivity(mPage)
+        }
+
     }
 
     private fun record() {
-        val recBtn = findViewById<Button>(R.id.contentRecodeButton)
+        val recBtn = findViewById<Button>(R.id.contentRecodeBtn)
 
         recBtn.setOnClickListener {
-            val rPage = Intent(this, MapsActivity::class.java)
+            val rPage = Intent(this, RecordActivity::class.java)
             startActivity(rPage)
         }
     }
 
-    private fun profile() {
-        val proBtn = findViewById<Button>(R.id.contentProfileButton)
+    private fun setting() {
+        val setBtn = findViewById<Button>(R.id.contentSettingBtn)
 
-        proBtn.setOnClickListener {
-            val pPage = Intent(this, ProfileActivity::class.java)
-            startActivity(pPage)
+        setBtn.setOnClickListener {
+            val sPage = Intent(this, ProfileActivity::class.java)
+            startActivity(sPage)
         }
     }
 
